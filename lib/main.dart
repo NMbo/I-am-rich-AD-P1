a416224408cb0984b63669f73b10cd2a9993081a
333736 <90184972+NMbo@users.noreply.github.com>
2022-09-07 12:59:58 +0200

    Einde les 2

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Een mooie apps!'),
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
